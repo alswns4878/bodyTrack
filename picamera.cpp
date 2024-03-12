5edfaa8d50688adbed16e504eb8b4eefe862d06d
alswns4878 <118281560+alswns4878@users.noreply.github.com>
2024-03-12 22:18:43 +0900

    Update picamera.cpp

--- picamera.cpp
+++ picamera.cpp
@@ -19,10 +19,10 @@ int main() {
     std::vector<int> compression_params;
     compression_params.push_back(cv::IMWRITE_JPEG_QUALITY);
     compression_params.push_back(95);  // You can adjust the quality (0-100)
-
+    
     std::vector<uchar> encoded_image;
     cv::imencode(".jpg", image, encoded_image, compression_params);
-
+    
     // Save the compressed image to a file
     std::ofstream file("compressed_image.jpg", std::ios::binary);
     file.write(reinterpret_cast<const char*>(encoded_image.data()), encoded_image.size());
